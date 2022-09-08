0c49c0518d267075ca3c8f1123f823d2c2abd5eb
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-08 21:01:57 +0000

    Changed 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, str, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
