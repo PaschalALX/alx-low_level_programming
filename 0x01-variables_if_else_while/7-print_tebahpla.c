56796f002a65ae7ac961f15d733054ead1dbce38
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-09 16:43:46 +0000

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int z;
+
+	for (z = 'z'; z >= 'a'; z--)
+	{
+		putchar(z);
+	}
+	putchar('\n');
+	return (0);
+}
