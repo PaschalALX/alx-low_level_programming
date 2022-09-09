58d51bacdaebc1b066e5a36bb6ee00a91d6a543b
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-09 16:58:38 +0000

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int no;
+
+	for (no = '0'; no <= '9'; no++)
+	{
+		putchar(no);
+		if (no == '9')
+			break;	
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
