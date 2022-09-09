096f2090901c0be0266dc9d5bbfb1ae4b6403724
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-09 16:49:04 +0000

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int no, a;
+	for (no = '0'; no <= '9'; no++)
+	{
+		putchar(no);
+	}
+	for (a = 'a'; a <= 'f'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
