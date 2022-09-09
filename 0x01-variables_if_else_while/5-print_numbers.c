6034d0c03d8949ae065a6be763d027c561c4e56d
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-09 14:50:25 +0000

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int no;
+
+	for (no = 0; no <= 9; no++)
+	{
+		printf("%d", no);
+	}
+	putchar('\n');
+	return (0);
+}
