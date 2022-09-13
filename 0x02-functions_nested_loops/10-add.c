5d7513f75b89bfdf419cb8fa6af97fa4828dcd2e
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-13 19:57:21 +0000

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Return the addition of 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
