7e0ae6c54df1a633fe7e87e9e0af9ca90fa7ce13
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-13 20:11:59 +0000

    Added print to 98 update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,32 @@
 void print_to_98(int n)
 {
 	int i;
-
-	for (i = n; i <= 98; i++)
+	
+	if (n == 98)
 	{
-		printf("%d", i);
-		if (i == 98)
-			break;
-		_putchar(',');
-		_putchar(' ');
-
+		printf("%d", n);
 	}
+	else if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i == 98)
+				break;
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i == 98)
+				break;
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar(' ');
 }
