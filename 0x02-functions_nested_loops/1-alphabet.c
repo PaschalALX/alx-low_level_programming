f6209537b267d908ced942a51ddbb6cf490f2b6c
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-13 12:42:34 +0000

    Refactored Alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,14 @@
 #include "main.h"
 
+/**
+ * print_alphabet - Prints alphabets in small letters
+ *
+ * Return: void
+ */
 void print_alphabet(void)
 {
 	char alpha = 'a';
+	
 	while (1)
 	{
 		_putchar(alpha);
