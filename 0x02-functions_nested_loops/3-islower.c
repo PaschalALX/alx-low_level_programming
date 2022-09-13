23834406aa8df72a55a5e91a779bf57e5d8f92bd
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-13 14:06:10 +0000

    created 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - Checks if character is in lower case
+ *
+ * Return: int
+ */
+int _islower(int c)
+{
+	int res = 0;
+	int alpha = 'a';
+
+	while (1)
+	{
+		if (c == alpha)
+			res = 1;
+		if (alpha == 'z')
+			break;
+		alpha++;
+	}
+	return res;
+}
