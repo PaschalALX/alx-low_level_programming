1f2c4636729450e1c3d021aaef0ade825d7f5053
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-13 23:15:21 +0000

    101 - natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if(((a % 3) == 0) || ((a % 5) == 0))
+		{
+			b += a;
+		}	
+	}
+	printf("%d", b);
+}
