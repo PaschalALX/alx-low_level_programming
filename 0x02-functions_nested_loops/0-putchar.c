65815ba73571e6258718ef0584af4a3e731ca456
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-13 10:08:46 +0000

    Started functions anf nested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char * str = "_putchar";
+	int i = 0;
+
+	while (1)
+	{
+		_putchar(str[i]);
+		if (i == 7)
+			break;
+		i++;
+	}
+	_putchar(10);
+	return (0);
+}
