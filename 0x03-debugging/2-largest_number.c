ba79c71373788a4d68a7b7edd0691da40f8daeef
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-14 19:39:42 +0000

    Debugged 2-largest_number x 4

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,18 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b > a && b > c)
 	{
 		largest = b;
 	}
-	else if (c > b && b > a)
+	else
 	{
 		largest = c;
 	}
+
 	return (largest);
 }
