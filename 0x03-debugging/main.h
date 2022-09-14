5dfabb777eee9820811026508a4a6df54bb6ab43
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-14 20:54:20 +0000

    Debugged print remaining days

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,4 +6,5 @@
 void positive_or_negative(int i);
 long largest_number(long a, long b, long c);
 void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 #endif /* MAIN_H */
