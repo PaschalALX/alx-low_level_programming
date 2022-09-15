a46f13287754423f235eef5f1f3a9de66ed24baf
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-15 20:09:28 +0000

    prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	unsigned long no = 612852475143;
+	unsigned long breakdown = no;
+	unsigned long buildup = 1;
+	int divisor = 2;
+
+	while (1)
+	{
+		if ((breakdown % divisor) == 0)
+		{
+			breakdown /= divisor;
+			buildup *= divisor;
+		}
+		else
+			divisor++;
+		if (buildup >= no)
+			break;
+	}
+	printf("%d", divisor);
+
+	return (0);
+}
