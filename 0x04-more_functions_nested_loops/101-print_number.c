69021c6cf37cd45c88330725a35876db46c50a4f
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-15 20:37:58 +0000

    Final Commit

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - prints numbers
+ * @n: number to be printed
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar((n % 10) + '0');
+	_putchar(10);
+	}
+}
