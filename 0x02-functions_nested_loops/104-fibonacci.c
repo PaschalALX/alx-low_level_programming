5e0f706e94827ec8006870ceb376aef2a76b12ac
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-15 06:38:58 +0000

    Refactored

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,23 +7,35 @@
  */
 int main(void)
 {
-	unsigned long prev = 1;
-	unsigned long curr = 2;
-	unsigned long sum, i;
+	int counter, overflow;
+	unsigned long a = 1;
+	unsigned long b = 1;
+	unsigned long sum = 0;
+	long a_head, a_tail, b_head, b_tail, sum_head, sum_tail;
 
-	printf("%lu, %lu, ", prev, curr);
-
-	for (i = 2; i < 99; i++)
+	printf("1");
+	for (counter = 2; counter < 93; counter++) /* will still print */
 	{
-		sum = curr + prev;
-		prev = curr;
-		curr = sum;
-		printf("%lu", sum);
-		if (i == 98)
-			break;
-		printf(", ");
+		sum = a + b;
+		a = b;
+		b = sum;
+		printf(", %lu", sum);
+	}
+	a_head = a / 1000000000; /* break larger num into 2 parts */
+	a_tail = a % 1000000000;
+	b_head = b / 1000000000;
+	b_tail = b % 1000000000;
+	for (; counter < 99; counter++)
+	{
+		overflow = (a_tail + b_tail) / 1000000000;
+		sum_tail = (a_tail + b_tail) - (1000000000 * overflow);
+		sum_head = (a_head + b_head) + overflow;
+		printf(", %lu%lu", sum_head, sum_tail);
+		a_head = b_head;
+		a_tail = b_tail;
+		b_head = sum_head;
+		b_tail = sum_tail;
 	}
 	putchar(10);
-
 	return (0);
 }
