bda891f35a41c1c0d584fa981dad18038a2c1396
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-15 06:20:48 +0000

    Debug 3 x 4

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,10 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+	/*
+	*if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+	*/
+	if ((year % 4 == 0 || year % 400 == 0) && (year % 100 == 0))
 	{
 		if (month > 2)
 			day++;
@@ -24,7 +27,7 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
-		
+	
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
