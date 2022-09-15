c0982fc2b91a54fb00500e5ec8bacf75bede8ea9
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-15 20:54:09 +0000

    DONE

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,14 +9,29 @@
 
 void print_number(int n)
 {
-	if (n < 0)
+	unsigned int tens, digit, positive = n;
+	double t = 1;
+
+	if (n == 0)
+		_putchar('0');
+	else
 	{
-		_putchar('-');
-		n = -n;
+		if (n < 0)
+		{
+			positive = n * -1;
+			_putchar('-');
+		}
+
+		while (t <= positive)
+			t *= 10;
+		tens = t / 10;
+
+		while (tens >= 1)
+		{
+			digit = positive / tens;
+			_putchar(digit + '0');
+			positive = (positive - (tens * digit));
+			tens /= 10;
+		}
 	}
-
-	if (n / 10)
-		print_number(n / 10);
-
-	_putchar((n % 10) + '0');
 }
