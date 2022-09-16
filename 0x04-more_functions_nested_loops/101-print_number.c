1c83c372df34cfb803a8e3aa5b7dddb68b9f4c3b
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-16 20:09:25 +0000

    Working]

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,11 +9,11 @@
 
 void print_number(int n)
 {
-	int n_copy = n;
-	int multi_tens = 1;
-	int digit, diff;
+	unsigned int n_copy = n;
+	unsigned int multi_tens = 1;
+	unsigned int digit, diff;
 
-	if (n < 0 || n == -0)
+	if (n < 0)
 	{
 		n = -n;
 		_putchar('-');
