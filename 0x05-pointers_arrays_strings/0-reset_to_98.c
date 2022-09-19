55ef662adae11c293cc2c9a5e1c13dd99cfd0630
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-19 15:50:22 +0000

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Resets n
+ * @n: Int parameter
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void _puts(char *str);
+void rev_string(char *s);
+void puts2(char *str);
+
+
+#endif
