77190d3a95d7f4bdefb8357bbbe1912999e92ee7
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-19 18:12:59 +0000

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_array - prints elements of an array
+ * @a: array
+ * @n: number of elements to be printed
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i == (n - 1))
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+}
