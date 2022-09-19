e48f7dbd369a1712f166714120f3089b8edad6af
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-19 15:58:31 +0000

    TASK 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - Gets length of string
+ * @s: string parameter
+ *
+ * Return: Integer
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
