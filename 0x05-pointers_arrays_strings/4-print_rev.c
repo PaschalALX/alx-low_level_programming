28013601b518fd1bd4966c5ecf3d9291c645b4d3
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-19 16:45:52 +0000

    TASK 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_rev - prints string in reverse order
+ * @s: string parameter
+ *
+ * Return: Void
+ */
+
+void print_rev(char *s)
+{
+	int len, i;
+
+	len = strlen(s);
+	for (i = (len - 1); i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
