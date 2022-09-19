f33fe2c412b1fa1d471f4028298a592ede3364be
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-19 16:53:16 +0000

    TASK 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - reverse string
+ * @s: string parameter
+ *
+ * Return: Void
+ */
+
+void rev_string(char *s)
+{
+	char str_cpy[strlen(s) + 1], i, j;
+
+	j = 0;
+	for (i = (strlen(s) - 1); i >= 0; i--)
+	{
+		str_cpy[i] = s[j];
+		j++;
+	}
+	str_cpy[strlen(s)] = '\0';
+	strcpy(s, str_cpy);
+}
