b18da9acac7675a5f091c01e5efd065c360c7e7b
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-21 14:56:44 +0000

    Task 4 (Review 1)

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: array og ints
+ * @n: int
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int temp, start, end;
+
+	start = 0;
+	end = n - 1;
+
+	while (start != end)
+	{
+		temp = a[start];
+		a[start] = a[end];
+		a[end] = temp;
+		start++, end--;
+	}
+	a[n] = '\0';
+}
