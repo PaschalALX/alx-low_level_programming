0c8a18fa45b8a75277edd759e2d209789054588d
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-22 07:15:32 +0000

    Debugged task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -29,13 +29,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i]; i++)
+	for (i = 0; i <= _strlen(s1); i++)
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
-
-		if (_strlen(s1) != _strlen(s2))
-			return (_strlen(s1) - _strlen(s2));
 	}
 	return (0);
 }
