2c5982b682032943d5add988efa118b299a19893
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-22 14:11:18 +0000

    Refact task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,42 +1,24 @@
-#include <stdio.h>
-#include <string.h>
-
 /**
- * _strlen - gets length of string
- * @str: string
- *
- * Return: int
+ * leet - encode string into 1337 leet
+ * @s: string to manipulate
+ * Return: string
  */
-int _strlen(char *str)
-{
-	int len, i;
 
-	len = 0;
-	for (i = 0; str[i]; i++)
-		len++;
-	return (len);
-}
-
-/**
- * leet - encodes a string into 1337
- * @str: string
- *
- * Return: char
- */
-char *leet(char *str)
+char *leet(char *s)
 {
-	int english[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	int leet[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	int a[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int b[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
 	int i, j;
 
-	for (i = 0; i <= _strlen(str); i++)
+	for (j = 0; s[j] != '\0'; j++)
 	{
-		for (j = 0; j < 11; j++)
+		for (i = 0; a[i] != '\0'; i++)
 		{
-			if (str[i] == english[j])
-				str[i] = leet[j];
+			if (s[j] == a[i])
+				s[j] = b[i];
 		}
 	}
-	return (str);
+	return (s);
 }
