aa755381dbe2387d77d8a9549dbb170f488ae498
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-23 02:27:38 +0000

    Old Code for 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,62 +1,89 @@
 #include <stdio.h>
-
 /**
- * println - Prints single line
+ * first_func - 1st function needed to run print_buffer
  * @b: buffer
- * @len: length of string
- * @start: starting position of each line
+ * @k: int
+ * @l: int
+ * @i: int
  *
  * Return: void
  */
-void println(char *b, int len, int start)
+void first_func(char *b, int k, int l, int i)
 {
-	int i;
+k = (i / 10) * 10;
+for (l = k; l <= i; l++)
+{
+if (b[l] >= 32 && b[l] <= 126)
+putchar(b[l]);
+else
+putchar('.');
+}
 
-	printf("%08x: ", start);
-	for (i = start; i <= start + 9; i++)
-	{
-		if (len < 9 && i > start + len)
-			printf("  ");
-		else
-			printf("%02x", b[i]);
-		if (i % 2)
-			putchar(' ');
-	}
+putchar(10);
+printf("%08x: ", i + 1);
+}
 
-	for (i = start; i <= start + len; i++)
-	{
-		if (b[i] >= 32 && b[i] <= 126)
-			putchar(b[i]);
-		else
-			putchar('.');
-	}
-	printf("\n");
+/**
+ * sec_func - second function needed to run print_buffer
+ * @b: buffer
+ * @l: int
+ * @l_phase: int
+ * @size: int
+ */
+void sec_func(char *b, int l, int l_phase, int size)
+{
+for (l = l_phase; l <= (l_phase + ((l_phase + 9) - size)); l++)
+{
+putchar(' ');
+putchar(' ');
+}
+putchar(' ');
+putchar(' ');
+for (l = l_phase; l < size; l++)
+{
+if (b[l] >= 32 && b[l] <= 126)
+putchar(b[l]);
+else
+putchar('.');
+}
 }
 
 /**
  * print_buffer - prints buffer
  * @b: buffer
- * @size: size of buffer
+ * @size: buffer size
  *
  * Return: void
  */
 void print_buffer(char *b, int size)
 {
-	int i;
+int i, j, k, l, z;
+int l_phase;
 
-	if (size <= 0)
-		putchar(10);
+l_phase = z = (size / 10) * 10;
+j = 1;
+l = k = 0;
 
-	for (i = 0; i < size; i++)
-	{
-		if (!(i % 10) && !(i >= (size / 10) * 10))
-		{
-			println(b, 9, i);
-		}
-		else if (i >= (size / 10) * 10)
-		{
-			println(b, size - i - 1, i);
-			break;
-		}
-	}
+for (i = 0; i < size; i++)
+{
+if (i == 0)
+printf("%08x: ", i);
+printf("%02x", b[i]);
+
+if (!(j % 2))
+putchar(' ');
+
+if (((i % 10 == 9) && i > 0))
+{
+first_func(b, k, l, i);
+}
+else if (i == size - 1)
+{
+sec_func(b, l, l_phase, size);
+}
+j++;
+}
+putchar('\n');
+if (size == 0)
+putchar('\n');
 }
