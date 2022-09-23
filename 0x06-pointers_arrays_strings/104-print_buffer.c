dfd0b74f2c03f37315473330f72f0dd6db913eca
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-23 01:56:15 +0000

    Refactored codebase for 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,58 +1,59 @@
-#include "main.h"
 #include <stdio.h>
+
 /**
- * print_line - prints a s bytes of a buffer
- * @c: buffer to print
- * @s: bytes of buffer to print
- * @l: line of buffer to print
+ * print_line - Prints single line
+ * @b: buffer
+ * @len: length of string
+ * @start: starting position of each line
  *
  * Return: void
  */
+void print_line(char *b, int len, int start)
+{
+	int i;
 
-void print_line(char *c, int s, int l)
-{
-int j, k;
-for (j = 0; j <= 9; j++)
-{
-if (j <= s)
-printf("%02x", c[l * 10 + j]);
-else
-printf("  ");
-if (j % 2)
-putchar(' ');
-}
-for (k = 0; k <= s; k++)
-{
-if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-putchar(c[l * 10 + k]);
-else
-putchar('.');
-}
+	printf("%08x: ", start);
+	for (i = start; i <= start + 9; i++)
+	{
+		if (len < 9 && i > start + len)
+			printf("  ");
+		else
+			printf("%02x", b[i]);
+		if (i % 2)
+			putchar(' ');
+	}
+
+	for (i = start; i <= start + len; i++)
+	{
+		if (b[i] >= 32 && b[i] <= 126)
+			putchar(b[i]);
+		else
+			putchar('.');
+	}
+	printf("\n");
 }
 
 /**
- * print_buffer - prints a buffer
- * @b: buffer to print
+ * print_buffer - prints buffer
+ * @b: buffer
  * @size: size of buffer
  *
  * Return: void
  */
 void print_buffer(char *b, int size)
 {
-int i;
-for (i = 0; i <= (size - 1) / 10 && size; i++)
-{
-printf("%08x: ", i * 10);
-if (i < size / 10)
-{
-print_line(b, 9, i);
-}
-else
-{
-print_line(b, size % 10 - 1, i);
-}
-putchar('\n');
-}
-if (size == 0)
-putchar('\n');
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (!(i % 10) && !(i >= (size / 10) * 10))
+		{
+			print_line(b, 9, i, i);
+		}
+		else if (i >= (size / 10) * 10)
+		{
+			print_line(b, size - i - 1, i, i);
+			break;
+		}
+	}
 }
