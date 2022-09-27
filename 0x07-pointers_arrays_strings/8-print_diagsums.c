8edcddaec49691cf0086eeb723b159a75a119b4c
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-27 13:02:33 +0000

    Test algo for task 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - prints the sum of the two diagonals
- * of a square matrix of integers
+ * print_diagsums - prints the sum of the two
+ * diagonals of a square matrix of integers
+ *
  * @a: square ints
  * @size: size of square
  *
@@ -11,16 +12,18 @@
 void print_diagsums(int *a, int size)
 {
 	unsigned int sum_1, sum_2;
+	int end_of_buffer, step;
 	int i;
 
 	sum_1 = sum_2 = 0;
+	end_of_buffer = size * size;
+	step = size + 1;
 
-	for (i = 0; i < size; i++)
-	{
-		sum_1 += a[(size * i) + i];
-		sum_2 += a[(size * (i + 1)) - (i + 1)];
-	}
+	for (i = 0; i < end_of_buffer; i += step)
+		sum_1 += a[i];
+
+	for (i = end_of_buffer - size; i >= 0; i -= (size - 1))
+		sum_2 += a[i];
 
 	printf("%d, %d\n", sum_1, sum_2);
 }
-
