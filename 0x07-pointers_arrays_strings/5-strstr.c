de6e173ecaf8ee2a604bee3c1a8871736e04bc20
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-09-27 17:28:36 +0000

    Refactored 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,37 +10,22 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int idx, i, j, k, needle_len;
+	int i, j, k, needle_len;
 
 	if (!strlen(needle))
 		return (haystack);
 
-	idx = -1;
 	needle_len = (int) strlen(needle);
+
 	for (i = 0; *(haystack + i); i++)
-	{
-		if (*needle == *(haystack + i))
+	{	if (*needle == *(haystack + i))
 		{
-
 			for (k = 0, j = i; k < needle_len; j++, k++)
-			{
 				if (*(needle + k) != *(haystack + j))
-				{
-					k = 0;
 					break;
-				}
 				else if ((k == needle_len - 1) && (*(needle + k) == *(haystack + j)))
-				{
-					idx = i;
-					break;
-				}
-			}
+					return (haystack + i);
 		}
-		if (idx >= 0)
-			break;
 	}
-
-	if (idx >= 0)
-		return (haystack + idx);
 	return (NULL);
 }
