d9356a0847e46761231a4df41908afeaa621d65d
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-10-03 17:43:30 +0000

    done 4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+
+
+
+/**
+ * free_grid - Free up memory allocated to grid
+ * @grid: grid
+ * @height: height of grid
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
