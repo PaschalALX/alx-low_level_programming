53237ba21a3b97c1c8d6c1169256a666dfd650cf
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-10-03 17:26:37 +0000

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - creates new copy of string
+ * @str: string to be copied.
+ *
+ * Return: pointer to char.
+ */
+char *_strdup(char *str)
+{
+	char *new_str;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	new_str = (char *) malloc(sizeof(char) * (strlen(str) + 1));
+
+	if (new_str == NULL)
+		return (NULL);
+
+	for (i = 0; i <= strlen(str); i++)
+	{
+		*(new_str + i) = *(str + i);
+	}
+	return (new_str);
+}
