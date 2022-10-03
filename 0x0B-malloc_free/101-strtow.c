a42b7929acd9668a88d2d36c1c3541c7ab3ecbbd
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-10-03 21:18:23 +0000

    Done 101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * word_count - counts the number of words in a str
+ * @str: string/sentence
+ *
+ * Return: int
+ */
+int word_count(char *str)
+{
+	int wc, i;
+
+	if (str[0] != ' ')
+		wc++;
+
+	i = 0;
+	while (str[i])
+	{
+		if ((str[i] == ' ' && str[i + 1] != ' '))
+			wc++;
+		i++;
+	}
+	return (wc);
+}
+
+/**
+ * parse_str - gets the words out of the string
+ * @str: string/sentence
+ * @words: array of words to replace
+ * @word_me_alloc: word memory allocation
+ *
+ * Return: char **
+ */
+char **parse_str(char *str, char **words, int word_me_alloc)
+{
+	int i, j, k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+
+	while (str[i])
+	{
+		if (str[i] == ' ' && str[i + 1] == ' ')
+			i++;
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+		{
+			words[j][k] = str[i];
+			k++;
+			words[j] = (char *) realloc(words[j], sizeof(char) * ++word_me_alloc);
+			words[j][k] = '\0';
+
+			k = 0;
+			word_me_alloc = 0;
+			j++;
+		}
+		if (str[i] != ' ' && (str[i + 1] != ' ' && str[i + 1] != '\0'))
+		{
+			words[j][k] = str[i];
+			words[j] = (char *) realloc(words[j], sizeof(char) * ++word_me_alloc);
+			k++;
+		}
+		i++;
+	}
+	return (words);
+}
+
+/**
+ * strtow - splits a string into words
+ * @str: string/sentence
+ *
+ * * Return: char **
+ */
+char **strtow(char *str)
+{
+	char **words;
+	int wc = word_count(str) + 1;
+	int w_ma = 1;
+	int i;
+
+	if (str == NULL || str == "")
+		return (NULL);
+
+	words = (char **) malloc(sizeof(char *) * wc);
+
+	for (i = 0; i < wc; i++)
+	{
+		words[i] = (char *) malloc(sizeof(char) * w_ma);
+	}
+
+	if (!words)
+		return (NULL);
+
+	words = parse_str(str, words, w_ma);
+	words[wc - 1] = NULL;
+
+	return (words);
+}
