916f5ca4d80d6eb0b18aaa49ec3b8ec2ad04d1ee
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-10-06 20:35:19 +0000

    added 0-malloc_checked.c *2

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,13 +9,12 @@
 
 void *malloc_checked(unsigned int b)
 {
-	char *ptr;
+	void *ptr;
 
 	ptr = malloc(b);
 
 	if (ptr == NULL)
 		exit(98);
-	else
-		return (ptr);
-	free(ptr);
+
+	return (ptr);
 }
