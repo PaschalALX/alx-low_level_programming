abbabbbe2303d16dced54933b5a18e2ebfc99d79
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-10-06 18:54:53 +0000

    added 0-malloc_checked.c

---
+++ 0x0c-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: buffer size
+ *
+ * Return: void *
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *m_ptr;
+
+	m_ptr = malloc(b);
+
+	if (!m_ptr)
+		exit(98);
+
+	return (m_ptr);
+}
