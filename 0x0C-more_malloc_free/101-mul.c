02c937fbd0fe6e2fa0e61826f65135fae65c5708
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-10-07 10:35:20 +0000

    test 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,199 +1,148 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * error_checks - Checks for errors
- * @argc: argument count
- * @argv: argument vector
+ * _print - moves a string one place to the left and prints the string
+ * @str: string to move
+ * @l: size of string
  *
  * Return: void
  */
-void error_checks(int argc, char **argv)
+void _print(char *str, int l)
+{
+	int i, j;
+
+	i = j = 0;
+	while (i < l)
+	{
+		if (str[i] != '0')
+			j = 1;
+		if (j || i == l - 1)
+			putchar(str[i]);
+		i++;
+	}
+
+	putchar('\n');
+	free(str);
+}
+
+/**
+ * mul - multiplies a char with a string and places the answer into dest
+ * @n: char to multiply
+ * @num: string to multiply
+ * @num_index: last non NULL index of num
+ * @dest: destination of multiplication
+ * @dest_index: highest index to start addition
+ *
+ * Return: pointer to dest, or NULL on failure
+ */
+char *mul(char n, char *num, int num_index, char *dest, int dest_index)
+{
+	int j, k, mul, mulrem, add, addrem;
+
+	mulrem = addrem = 0;
+	for (j = num_index, k = dest_index; j >= 0; j--, k--)
+	{
+		mul = (n - '0') * (num[j] - '0') + mulrem;
+		mulrem = mul / 10;
+		add = (dest[k] - '0') + (mul % 10) + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+	for (addrem += mulrem; k >= 0 && addrem; k--)
+	{
+		add = (dest[k] - '0') + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+	if (addrem)
+	{
+		return (NULL);
+	}
+	return (dest);
+}
+/**
+ * check_for_digits - checks the arguments to ensure they are digits
+ * @av: pointer to arguments
+ *
+ * Return: 0 if digits, 1 if not
+ */
+int check_for_digits(char **av)
+{
+	int i, j;
+
+	for (i = 1; i < 3; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			if (av[i][j] < '0' || av[i][j] > '9')
+				return (1);
+		}
+	}
+	return (0);
+}
+
+/**
+ * init - initializes a string
+ * @str: sting to initialize
+ * @l: length of strinf
+ *
+ * Return: void
+ */
+void init(char *str, int l)
 {
 	int i;
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	for (i = 0; i < (int)strlen(argv[1]); i++)
-	{
-		if (!(argv[1][i] >= '0' && argv[1][i] <= '9'))
-		{
-			printf("Error\n");
-			exit(98);
-		}
-	}
-
-	for (i = 0; i < (int)strlen(argv[2]); i++)
-	{
-		if (!(argv[2][i] >= '0' && argv[2][i] <= '9'))
-		{
-			printf("Error\n");
-			exit(98);
-		}
-	}
+	for (i = 0; i < l; i++)
+		str[i] = '0';
+	str[i] = '\0';
 }
 
 /**
- * __atoi - convert ascii char to int
- * @c: ascii char
- *
- * Return: int
- */
-int __atoi(char c)
-{
-	return (c - '0');
-}
-
-/**
- * __itoa - converts int to ascii char
- * @i: integer
- *
- * Return: char
- */
-char __itoa(int i)
-{
-	return ('0' + i);
-}
-
-/**
- * rev - reverse string
- * @str: string to reverse
- *
- * Return: char*
- */
-char *rev(char *str)
-{
-	int start, end, tmp;
-
-	start = 0;
-	end = strlen(str) - 1;
-
-	while (start < end)
-	{
-		tmp = str[start];
-		str[start] = str[end];
-		str[end] = tmp;
-		start++, end--;
-	}
-	return (str);
-}
-
-/**
- * exec - executes main instructions
- * @shorter: shorter argument
- * @longer: longer argument
- * @buf_len: buffer length
- * @buf: buffer
- *
- * Return: void
- */
-void exec(char *shorter, char *longer, int buf_len, char *buf)
-{
-	int i, j, k, l, of, mul, sum, sof;
-
-	mul = of = sof = sum = k = l = 0;
-	for (i = strlen(shorter) - 1; i >= 0; i--)
-	{
-		for (j = strlen(longer) - 1; k < buf_len; j--, k++)
-		{
-			if (j >= 0)
-				mul = __atoi(shorter[i]) * __atoi(longer[j]);
-
-			mul += of;
-			of = mul / 10;
-
-			if (mul == 0 && sof == 0 && j < 0)
-				break;
-
-			if (!(buf[k] >= '0' && buf[k] <= '9'))
-				buf[k] = '0';
-
-			sum += sof;
-			sum += __atoi(buf[k]) + (mul % 10);
-			sof = sum / 10;
-			buf[k] = __itoa(sum % 10);
-			sum = mul = 0;
-		}
-		if (i == 0 && j < 0 && sof == 0 && of == 0)
-			buf[k] = '\0';
-
-		l++;
-		of = sof = 0;
-		k = l;
-	}
-	printf("%s\n", rev(buf));
-	free(buf);
-}
-
-/**
- * check_zeros - checks for zeros
- * @a: first string
- * @b: second string
- *
- * Return: void
- */
-void check_zeros(char *a, char *b)
-{
-	int i = 0;
-	int flag1 = 1;
-	int flag2 = 1;
-
-	for (i = 0; i < (int) strlen(a); i++)
-	{
-		if (a[i] != '0')
-		{
-			flag1 = 0;
-			break;
-		}
-	}
-	for (i = 0; i < (int) strlen(b); i++)
-	{
-		if (b[i] != '0')
-		{
-			flag2 = 0;
-			break;
-		}
-	}
-	if (flag1 || flag2)
-	{
-		printf("0\n");
-		exit(0);
-	}
-}
-/**
- * main - Entry point
- * @argc: argument count
+ * main - multiply two numbers
+ * @argc: number of arguments
  * @argv: argument vector
  *
- * Return: int
+ * Return: zero, or exit status of 98 if failure
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	char *buf, *longer, *shorter;
-	int buf_len;
+	int l1, l2, ln, ti, i;
+	char *a;
+	char *t;
+	char e[] = "Error\n";
 
-	error_checks(argc, argv);
-	check_zeros(argv[1], argv[2]);
-
-	if (strlen(argv[1]) >= strlen(argv[2]))
+	if (argc != 3 || check_for_digits(argv))
 	{
-		longer = argv[1];
-		shorter = argv[2];
+		for (ti = 0; e[ti]; ti++)
+			putchar(e[ti]);
+		exit(98);
 	}
-	else
+	for (l1 = 0; argv[1][l1]; l1++)
+		;
+	for (l2 = 0; argv[2][l2]; l2++)
+		;
+	ln = l1 + l2 + 1;
+	a = malloc(ln * sizeof(char));
+	if (a == NULL)
 	{
-		longer = argv[2];
-		shorter = argv[1];
+		for (ti = 0; e[ti]; ti++)
+			putchar(e[ti]);
+		exit(98);
 	}
-
-	buf_len = (int) strlen(longer) * 2;
-	buf = malloc(sizeof(char) * buf_len);
-
-	exec(shorter, longer, buf_len, buf);
+	init(a, ln - 1);
+	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
+	{
+		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
+		if (t == NULL)
+		{
+			for (ti = 0; e[ti]; ti++)
+				putchar(e[ti]);
+			free(a);
+			exit(98);
+		}
+	}
+	_print(a, ln - 1);
 	return (0);
 }
