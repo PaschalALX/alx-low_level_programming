0ced45af24d98048d5febbc743e69f0336440211
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-10-10 11:48:17 +0000

    added 2-print_dog.c x 4

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,9 +12,9 @@ void print_dog(struct dog *d)
 	if (!d)
 		return;
 
-	!d->name ? printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
+	!d->name ? printf("Name: %p\n", d->name) : printf("Name: %s\n", d->name);
 
-	!d->age ? printf("Age: (nil)\n") : printf("Age: %f\n", d->age);
-
-	!d->owner ? printf("Owner: (nil)\n") : 	printf("Owner: %s\n", d->owner);
+	!d->age ? printf("Age: (nil)") : printf("Age: %f\n", d->age);
+	
+	!d->owner ? printf("Owner: %p\n", d->owner) : printf("Owner: %s\n", d->owner);
 }
