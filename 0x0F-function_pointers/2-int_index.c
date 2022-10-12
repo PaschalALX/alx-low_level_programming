d7266d8e2b9ab6b016f1a730f2c466d19b36da1f
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-10-12 14:28:57 +0000

    Added Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,10 +13,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	if (array && cmp)
 	{
-		if (cmp(array[i]))
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
 	}
+
 	return (-1);
 }
