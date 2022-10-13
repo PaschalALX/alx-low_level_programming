39e63533117491e67c606f385f09912228a2d5c5
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-10-13 17:02:02 +0000

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,32 +14,35 @@ void print_all(const char * const format, ...)
 	char *str, *str_fmt = "%p";
 	va_list args;
 
-	va_start(args, format);
-	while (format[++i])
+	if (format)
 	{
-		switch (format[i])
+		va_start(args, format);
+		while (format[++i])
 		{
-			case 'c':
-				printf("%c", va_arg(args, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(args, double));
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				if (str)
-					str_fmt = "%s";
-				printf(str_fmt, str);
-				break;
-			default:
-				continue;
+			switch (format[i])
+			{
+				case 'c':
+					printf("%c", va_arg(args, int));
+					break;
+				case 'i':
+					printf("%d", va_arg(args, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(args, double));
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (str)
+						str_fmt = "%s";
+					printf(str_fmt, str);
+					break;
+				default:
+					continue;
+			}
+			if (format[i + 1])
+				printf(", ");
 		}
-		if (format[i + 1])
-			printf(", ");
+		va_end(args);
 	}
-	va_end(args);
 	printf("\n");
 }
