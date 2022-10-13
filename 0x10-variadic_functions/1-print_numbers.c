7662e6bcc32806cb07427dc3da96ac6abdb6f778
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-10-13 14:12:56 +0000

    Amended 1-print_numbers.c x 2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,7 +13,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i = n;
 
-	if (!separator || !n)
+	if (!separator)
 		return;
 
 	va_start(args, n);
