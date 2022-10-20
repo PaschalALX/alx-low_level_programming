12f871812e891612648e745aa95296a00821f23a
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-10-20 13:47:46 +0000

    Added 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void before_main(void) __attribute__((constructor));
+void before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
