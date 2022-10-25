77e19bf96415d2d170e88ac200b6d60ba989490f
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-10-25 07:57:51 +0000

    9-indert_node * 4

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
         }
         while (prev)
         {
-                if (!ptr)
+                if (!prev->next)
 			return (NULL);
 
 		if (i == idx)
