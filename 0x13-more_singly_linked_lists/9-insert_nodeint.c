3674455bd6f8ce4b50ee3225ccc2d487891dfd0a
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-10-25 09:18:24 +0000

    9-indert_node * 10

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,7 +39,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
                    	return (new_node);
                 }
                 i++;
-                prev = ptr;
                 ptr = ptr->next;
         }
 
