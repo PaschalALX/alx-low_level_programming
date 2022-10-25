39596f17e9ff8a7eb4c579231dcec3dafa423d0b
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-10-25 11:40:26 +0000

    Added coorrect 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,37 +10,37 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-        listint_t *new_node;
-        listint_t *ptr = *head;
-        unsigned int i = 0;
+	listint_t *new_node;
+	listint_t *ptr = *head;
+	unsigned int i = 0;
 
-        if (!head)
-                return (NULL);
+	if (!head)
+		return (NULL);
 
-        new_node = malloc(sizeof(listint_t));
+	new_node = malloc(sizeof(listint_t));
 
-        if (!new_node)
-                return (NULL);
+	if (!new_node)
+		return (NULL);
 
-        new_node->n = n;
+	new_node->n = n;
 
-        if (!idx)
-        {
-                new_node->next = ptr;
-                (*head) = new_node;
-                return (new_node);
-        }
-        while (ptr)
-        {
+	if (!idx)
+	{
+		new_node->next = ptr;
+		(*head) = new_node;
+		return (new_node);
+	}
+	while (ptr)
+	{
 		if (i == idx - 1)
-                {
-                        new_node->next = ptr->next;
+		{
+			new_node->next = ptr->next;
 			ptr->next = new_node;
-                   	return (new_node);
-                }
-                i++;
-                ptr = ptr->next;
-        }
+			return (new_node);
+		}
+		i++;
+		ptr = ptr->next;
+	}
 
-        return (NULL);
+	return (NULL);
 }
