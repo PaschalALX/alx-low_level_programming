95649ce9c41df8bd1375cb1fb882e1abce41f9cc
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-10-25 07:00:54 +0000

    Refactored 9-insert_nodeint.c x 5

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *prev = NULL;
 	unsigned int i = 0;
 
-	if (!head || !(*head) || !(*head)->next)
+	if (!head || !(*head))
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
