263810a6708c3fae85e364d04c9941c9f9d57f30
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-10-27 17:46:03 +0000

    Added 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+
+/**
+ * get_bit - returns the value of a bit at a given index.
+ * @n: integer
+ * @index: index of bit to be returned
+ *
+ * Return: int
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (!index)
+		return (n & 1);
+
+	return (get_bit(n >> 1, index - 1));
+}
