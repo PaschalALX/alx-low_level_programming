2a435b78c4f8b95acb2f87becfec8ace7cc92f3d
Paschal-Mark Okafor <okaforpaschal018@gmail.com>
2022-10-27 17:24:02 +0000

    Refactored 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,49 +1,15 @@
 #include "main.h"
+
 /**
- * print_binary - prints the binary representation of a number.
- * @n: integer
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number to be printed in binary.
  *
  * Return: void
  */
 void print_binary(unsigned long int n)
 {
-	int check = 0;
-	unsigned long int n_cpy;
-	int expo, com_bindex;
-	char bit,  flag;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	n_cpy = n;
-	com_bindex = 1;
-	expo = bit = flag = 0;
-
-	if (!n)
-		_putchar('0');
-
-	while (n_cpy)
-	{
-		expo += 1;
-		n_cpy >>= 1;
-	}
-
-	while (expo)
-	{
-		com_bindex *= 2;
-		expo--;
-	}
-
-	while (com_bindex)
-	{
-		check = n & com_bindex;
-		bit = check ? '1' : '0';
-
-		if (bit == '0')
-		{
-			if ((bit == '0') && flag)
-				_putchar(bit);
-			flag = 1;
-		}
-		else
-			_putchar(bit);
-		com_bindex >>= 1;
-	}
+	_putchar((n & 1) + '0');
 }
